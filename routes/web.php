93114e93f38750035a7fa3302cf9e5e7a6ab9d16
carlosgomezcp <carlosgomezcp@gmail.com>
2023-10-31 01:04:59 -0400

    creado controlador

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin',[HomeController::class,'index'])->name('admin');
